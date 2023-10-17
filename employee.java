d42069ad78433ebd886065db22cfddc87f635b8d
Helen <hcardoso103@gmail.com>
2023-10-16 22:41:40 -0400

    Feat: Added a clock in method

--- employee.java
+++ employee.java
@@ -1,14 +1,17 @@
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.time.Duration;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileWriter;
 
 class Employee{
     private int employeeID;
     private String firstName;
     private String lastName;
-    private LocalTime clockInTime;
-    private LocalTime clockOutTime;
-    private LocalTime breakTime;
-    private LocalTime lunchTime;
+    private LocalDateTime clockInTime;
+    private LocalDateTime clockOutTime;
+    private LocalDateTime breakTime;
+    private LocalDateTime lunchTime;
     private Duration totalWorkHours;
     
 
@@ -16,8 +19,33 @@ class Employee{
         this.employeeID = employeeID;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.clockInTime = LocalTime.now();
-        this.clockOutTime = LocalTime.now();
-        this.breakTime = LocalTime.now();
+        this.clockInTime = LocalDateTime.now();
+        this.clockOutTime = LocalDateTime.now();
+        this.breakTime = LocalDateTime.now();
+        this.lunchTime = LocalDateTime.now();
+        this.totalWorkHours = Duration.ZERO;
+    }
+
+    public void clockIn(){
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        // Write to the clock_in_times.csv file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("hours/clock_in_times.csv", true))) {
+            writer.write(employeeID + "," + currentDateTime.toLocalDate() + "," + currentDateTime.toLocalTime() + "\n");
+            System.out.println("Clock-in time saved to clock_in_times.csv file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clockOut(){
+        this.clockOutTime = LocalDateTime.now();
+        this.totalWorkHours = this.totalWorkHours.plus(Duration.between(clockInTime, clockOutTime));
+    }
+
+    
+
+    // Getter for total work hours
+    public Duration getTotalWorkHours(){
+        return totalWorkHours;
     }
 }
--- main.java
+++ main.java
@@ -8,7 +8,31 @@ public class main {
         
         String id = login.nextLine();
 
-        System.out.println("Welcome, ");
+        // takes the string id and looks it up in a method to check if the ID # is in files
+
+        //returns Welcome Message
+
+        // The next options are:
+        // Clock In
+           // returns a new line that says "Clocked in at HH:MM:SS DAY/MONTH/YEAR"
+           // returns back to menu page
+           // if tries to clock in again returns "Already clocked in at HH:MM:SS DAY/MONTH/YEAR"
+        // Clock Out
+           // retuns: 
+              //Lunch
+                // returns "Clocked out for Lunch at HH:MM:SS DAY/MONTH/YEAR"
+                // if tries any of the opens except 'Clock In', returns "Already clocked out for lunch at HH:MM:SS DAY/MONTH/YEAR" 
+                // returns back to menu
+              //Break
+                // returns "Clocked out for Break at HH:MM:SS DAY/MONTH/YEAR"
+                // if tries any of the opens except 'Clock In', returns "Already clocked out for break at HH:MM:SS DAY/MONTH/YEAR"
+                // returns back to menu
+              //Clock Out
+                // returns "Clocked out at HH:MM:SS DAY/MONTH/YEAR"
+                // clocks back in if employee attemps so
+        // View Hours
+           // returns a table starting from sunday to saturday rows with date in/lunch/break/back in
+        // Exit 
 
 
     }
